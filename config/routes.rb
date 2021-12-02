0dafc916a697b06afafe994d6c3ed3c6be91e66d
leviathan-7 <m.grober02@yandex.ru>
2021-12-02 19:32:58 +0300

    update for api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/convertings/metric/:metric_name', to: "household#watch"
   post '/convertings/household/:household_name', to: "metric#watch"
-  get '/recipe?name=:recipe_name&food=:food_name&count=:metric_count', to: "recipe#watch"
+  get '/recipe?name=:recipe_name&food=:food_name&convertings=:convert&count=:count', to: "recipe#watch"
 end
