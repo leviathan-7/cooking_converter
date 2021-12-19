e5a83b9478598e95a5e9f380c5404228411b47b8
Grober Michael <58160994+leviathan-7@users.noreply.github.com>
2021-12-19 10:37:16 +0300

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#page'
-  get '/recipe', to "recipe#page"
-  get '/convertings?count=:count&from=:convert_from&to=:convert_to', to: "convertings#watch"
-  get '/recipe?name=:recipe_name&food=:food_name&convertings=:convert&count=:count', to: "recipe#watch"
-end
